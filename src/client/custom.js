322e9ba61d8811bf8659b66001443ae3935c9f7a
Lukas Taegert <lukas.taegert@tngtech.com>
2016-10-27 08:50:10 +0200

    Performance

--- src/client/custom.js
+++ src/client/custom.js
@@ -5,3 +5,11 @@ window.runCode = (id, id2) => {
   }
   return false;
 };
+
+window.measure = (prepareId, sourceId, printId) => {
+  eval(document.getElementById(prepareId).textContent);
+  const startTime = performance.now();
+  eval(document.getElementById(sourceId).textContent);
+  document.getElementById(printId).textContent += (performance.now() - startTime) + '\n';
+  return false;
+};
