64f164f18a5629b4e63b81c40cbad4efb70b50c9
Lukas Taegert <lukas.taegert@tngtech.com>
2016-11-16 06:48:35 +0100

    Implement basic Pixi management

--- src/client/custom.js
+++ src/client/custom.js
@@ -1,15 +1,308 @@
-window.runCode = (...ids) => {
-  eval(ids.map(id => document.getElementById(id).textContent).join(';'));
-  return false;
-};
+// === Add listeners that fire when a slide identified by data-slidename="<slideName>" enters or leaves view
+var addSlideListener = (function() {
+  var listenedSlides = [];
 
-window.measure = (prepareId, sourceId, printId) => {
-  eval(
-    (prepareId && document.getElementById(prepareId).textContent) +
-    'const startTime = performance.now();' +
-    document.getElementById(sourceId).textContent +
-    'const time = performance.now() - startTime;' +
-    'document.getElementById("' + printId + '").textContent += time + "\\n";'
-  );
-  return false;
-};
+  function onSlideChanged() {
+    var slideName = Reveal.getCurrentSlide().dataset.slidename;
+    listenedSlides.forEach(function(slide) {
+      if (slide.entered && slide.name !== slideName) {
+        slide.entered = false;
+        slide.onLeave && slide.onLeave();
+      }
+    });
+    listenedSlides.forEach(function(slide) {
+      if (slide.name === slideName) {
+        slide.entered = true;
+        slide.onEnter && slide.onEnter();
+      }
+    });
+  }
+
+  function onReady() {
+    Reveal.addEventListener('slidechanged', onSlideChanged);
+    onSlideChanged();
+  }
+
+  function waitForReady() {
+    if (Reveal.isReady()) {
+      return onReady();
+    }
+    setTimeout(waitForReady, 0);
+  }
+
+  waitForReady();
+
+  return function(slideName, onEnter, onLeave) {
+    listenedSlides.push({name: slideName, onEnter: onEnter, onLeave: onLeave});
+  }
+}());
+
+// === Special listener for slides containing Pixi canvas elements with resize detection
+var initPixiSlide = (function() {
+  var ownSlideName;
+  var enterHandler, teardownHandler, resizeHandler;
+  var renderer;
+  var origRequestAnimationFrame;
+  var isActive = false;
+  var requestReactivate = false;
+
+  function requestUpdateCanvasSize() {
+    console.log('request update size')
+    window.requestAnimationFrame(function() {
+      console.log('update size')
+      resizeHandler && resizeHandler();
+      renderer && renderer.resize(renderer.view.clientWidth, renderer.view.clientHeight);
+    });
+  }
+
+  function onEnter() {
+    origRequestAnimationFrame = window.requestAnimationFrame;
+    isActive = true;
+    console.log('replace RAF')
+    window.requestAnimationFrame = function() {
+      if (isActive) {
+        origRequestAnimationFrame.apply(this, arguments);
+      } else {
+        console.log('inactive frame')
+        origRequestAnimationFrame(function() {
+          if (requestReactivate) {
+            console.log('reactivate frame')
+            isActive = true;
+            requestReactivate();
+            requestReactivate = false;
+          } else {
+            console.log('restore frame')
+            window.requestAnimationFrame = origRequestAnimationFrame;
+          }
+        });
+      }
+    };
+    enterHandler && enterHandler();
+  }
+
+  function onLeave() {
+    teardownHandler && teardownHandler();
+    isActive = false;
+    requestReactivate = false;
+  }
+
+  function runCode(onRunCode) {
+    console.log('actually running code')
+    var origAutoDetectRenderer = PIXI.autoDetectRenderer;
+    PIXI.autoDetectRenderer = function() {
+      renderer = origAutoDetectRenderer.apply(this, arguments);
+      return renderer;
+    };
+
+    onRunCode();
+
+    PIXI.autoDetectRenderer = origAutoDetectRenderer;
+    requestUpdateCanvasSize();
+  }
+
+  function requestRunCode(onRunCode) {
+    console.log('request run code')
+    isActive = false;
+    requestReactivate = function() {
+      runCode(onRunCode)
+    };
+    window.requestAnimationFrame(function() {});
+  }
+
+  return function(slideName, onGetRunCodeHook, onEnterSlide, onTeardown, onResize) {
+    onGetRunCodeHook(requestRunCode);
+    enterHandler = onEnterSlide;
+    teardownHandler = onTeardown;
+    resizeHandler = onResize;
+    ownSlideName = slideName;
+    addSlideListener(slideName, onEnter, onLeave);
+  }
+}());
+
+// === Structure slide
+(function() {
+  var SLIDE_NAME = 'structure';
+
+  function onGetRunCodeHook(runCodeHook) {
+    console.log('set window.runCode')
+    window.runCode =function() {
+      var commands = Array.prototype.slice.call(arguments).map(function(id) {return document.getElementById(id).textContent});
+      runCodeHook(function () {eval(commands.join(';'));});
+      return false;
+    };
+  }
+
+  function onEnter() {
+  }
+
+  function onTeardown() {
+  }
+
+  function onResize() {
+  }
+
+  initPixiSlide(SLIDE_NAME, onGetRunCodeHook, onEnter, onTeardown, onResize);
+}());
+
+// === Initial presentation
+(function() {
+  var SLIDE_NAME = 'intro';
+  var runCode;
+  var canvas, renderer, stage, backgroundContainer, logoContainer;
+  var cats;
+  var isLogoRotating;
+
+  function onGetRunCodeHook(runCodeHook) {
+    runCode = runCodeHook;
+  }
+
+  function onRunCode() {
+    var MAX_CATS = 1000;
+    canvas = document.getElementById('canvas-' + SLIDE_NAME);
+    isLogoRotating = false;
+    cats = [];
+
+    renderer = new PIXI.autoDetectRenderer(canvas.clientWidth, canvas.clientHeight, {
+      transparent: true,
+      autoResize: false,
+      view: canvas
+    });
+
+    stage = new PIXI.Container();
+    backgroundContainer = new PIXI.Container();
+    stage.addChild(backgroundContainer);
+
+    var catContainer = new PIXI.Container();
+    stage.addChild(catContainer);
+
+    logoContainer = new PIXI.Container();
+    stage.addChild(logoContainer);
+
+    var loader = new PIXI.loaders.Loader()
+      .add('img/pixi-logo.png')
+      .add('img/cats.json')
+      .add('img/tng.png')
+      .once('complete', setupBackground)
+      .once('complete', setupPixiLogo)
+      .once('complete', performAnimationLoop)
+      .load();
+
+    function setupBackground() {
+      var tngLogo = createTNGLogo();
+      createTextAboveLogo(tngLogo);
+    }
+
+    function createTNGLogo() {
+      var tngLogo = PIXI.Sprite.fromFrame('img/tng.png');
+      tngLogo.scale.set(0.5, 0.5);
+      tngLogo.position.set(-tngLogo.width / 2, -tngLogo.height - 10);
+      backgroundContainer.addChild(tngLogo);
+      return tngLogo;
+    }
+
+    function createTextAboveLogo(tngLogo) {
+      var text = new PIXI.Text(
+        'Lukas Taegert', {font: '24px Arial', fill: 0x101010, align: 'center'}
+      );
+      text.anchor.set(0.5, 1);
+      text.position.set(0, -tngLogo.height - 28);
+      // using double resolution provides better rendered text when it is not perfectly aligned
+      text.resolution = 2;
+      backgroundContainer.addChild(text);
+    }
+
+    var currentCatSpriteIndex = 0;
+
+    function addCats(numberOfCats) {
+      for (var i = 0; i < numberOfCats; i++) {
+        var sprite = PIXI.Sprite.fromFrame(String(currentCatSpriteIndex));
+        resetCatSprite(sprite);
+        cats.push({sprite: sprite, speed: 1 + Math.random() * 4});
+        currentCatSpriteIndex = (currentCatSpriteIndex + 1) % 9;
+        catContainer.addChild(sprite);
+      }
+    }
+
+    function setupPixiLogo() {
+      createLogoSprite();
+      var title = createSubtitle();
+
+      logoContainer.on('mouseover', function() {
+        var colorMatrix = new PIXI.filters.ColorMatrixFilter();
+        colorMatrix.greyscale(0.5);
+
+        logoContainer.filters = [colorMatrix];
+      });
+
+      logoContainer.on('mouseout', function() {
+        logoContainer.filters = null;
+      });
+
+      logoContainer.on('click', function() {
+        if (!isLogoRotating) {
+          isLogoRotating = true;
+          logoContainer.removeChild(title);
+        } else {
+          console.log('cats.length', cats.length)
+          console.log('add', Math.min(Math.max(cats.length, 1), MAX_CATS - cats.length))
+          addCats(Math.min(Math.max(cats.length, 1), MAX_CATS - cats.length));
+        }
+      });
+    }
+
+    function createSubtitle() {
+      var title = new PIXI.Text('denn Dein Canvas will mehr…', {font: '48px Arial', fill: 0x101010, align: 'center'});
+      title.anchor.set(0.5, 0);
+      title.resolution = 2;
+      title.position.y = 80;
+      logoContainer.addChild(title);
+      return title;
+    }
+
+    function createLogoSprite() {
+      var logoSprite = PIXI.Sprite.fromFrame('img/pixi-logo.png');
+      logoSprite.position.set(-logoSprite.width / 2, -logoSprite.height / 2);
+      logoContainer.addChild(logoSprite);
+      // without this flag, mouse events will not be passed to this container
+      logoContainer.interactive = true;
+    }
+
+    function resetCatSprite(sprite) {
+      sprite.position.x = -sprite.width;
+      sprite.position.y = -sprite.height / 2 + Math.random() * canvas.clientHeight;
+    }
+
+    function updateStage() {
+      if (isLogoRotating) {
+        logoContainer.rotation += 0.01;
+      }
+      cats.forEach(function(cat) {
+        cat.sprite.position.x += cat.speed;
+        if (cat.sprite.position.x >= canvas.clientWidth) {
+          resetCatSprite(cat.sprite);
+        }
+      });
+    }
+
+    function performAnimationLoop() {
+      updateStage();
+      renderer.render(stage);
+      window.requestAnimationFrame(performAnimationLoop);
+    }
+  }
+
+  function onEnter() {
+    runCode && runCode(onRunCode);
+  }
+
+  function onTeardown() {
+  }
+
+  function onResize() {
+    logoContainer.position.set(canvas.clientWidth / 2, canvas.clientHeight / 2);
+    backgroundContainer.position.set(canvas.clientWidth / 2, canvas.clientHeight);
+  }
+
+  initPixiSlide(SLIDE_NAME, onGetRunCodeHook, onEnter, onTeardown, onResize);
+}());
+
--- workerFile.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('message', message => {
-  console.log('Received: ' + message.data);
-  for (let i = 0; i < 1000000000; i++) {
-    if (i % 100000000 === 0) self.postMessage(i);
-  }
-});
